858a006707b6ecd52ebf9544cf46e0f6195c188a
lielorenstein <57412592+lielorenstein@users.noreply.github.com>
2020-03-22 18:28:59 +0200

    18:28 22/03 exe2

---
+++ src/unittests/VectorTest.java
@@ -0,0 +1,46 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+public class VectorTest {
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void subtract() {
+    }
+
+    @org.junit.Test
+    public void testAdd() {
+    }
+
+    @org.junit.Test
+    public void scale() {
+    }
+
+    @org.junit.Test
+    public void dotProduct() {
+    }
+
+    @org.junit.Test
+    public void crossProduct() {
+    }
+
+    @org.junit.Test
+    public void lengthSquared() {
+    }
+
+    @org.junit.Test
+    public void length() {
+    }
+
+    @org.junit.Test
+    public void normalize() {
+    }
+
+    @org.junit.Test
+    public void normalized() {
+    }
+}
